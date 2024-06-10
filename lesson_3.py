19d60e0525c67bda7ce599bcc067fbf6093cab58
nikolaskozlan <maxsuccess4@gmail.com>
2024-06-10 19:09:45 +0300

    added 3rd lesson

---
+++ lesson_3.py
@@ -0,0 +1,10 @@
+# Bool, None types
+# Logic operators
+# Зведення типів
+# Duck typing
+# If statement (programming golf, Тернарний оператор)
+# list
+# змінні не змінні типи даних різниця
+# методи list(append(), pop(), sort(), copy())
+# функція sorted()
+
